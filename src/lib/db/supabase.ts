3fc704c02ba2e11d0e6e3b811f35b39d3d3702ef
Irvan Maulana <vanir1812@gmail.com>
2025-03-11 08:25:59 +0700

    migrate: removing supabase entirely

--- src/lib/db/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-import type { Database } from "@/lib/db/types";
-
-// Create a single supabase client for interacting with your database
-export const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_API_KEY!,
-);
