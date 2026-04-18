2a97714c547425f795cab0a5e573fda01cd87b74
Irvan Maulana Ahmad <vanir1812@gmail.com>
2026-04-18 16:52:24 +0700

    changes: replace next 15 with tanstack start, currently only starter app

--- src/lib/utils/invalid-webhook-type.ts
+++
@@ -1,9 +0,0 @@
-// this utility function purpose is only to check if the webhook type is valid or not
-// suspect is user is manually accessing the route
-import type { Webhook } from "@/lib/types/webhook";
-
-const expecedTypes = ["user.created", "user.updated", "user.deleted"];
-
-export function isValidWebhookType(webhook: Webhook) {
-  return expecedTypes.includes(webhook.type);
-}
